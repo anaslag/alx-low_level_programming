317edc3d59d53541ec99c7f30ecff9fd43054a66
anaslag <anas.lagnani@gmail.com>
2023-04-10 11:02:06 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * function main : prints program followed by a new line
+ * @argc : int
+ * @argv : list
+ * Return : 0
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
