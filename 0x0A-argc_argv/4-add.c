66143baf4e449ed60499a16a47c8ebcad0f76653
anaslag <anas.lagnani@gmail.com>
2023-04-10 12:09:39 -0700

    sum

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,12 +16,11 @@ int main(int argc, char *argv[])
 	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
-		{
-			if (*c < '0' || *c > '9')
+		{	if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
-			sum += atoi(argv[argc]);
 		}
+		sum += atoi(argv[argc]);
 	}
-	printf("%d/n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
