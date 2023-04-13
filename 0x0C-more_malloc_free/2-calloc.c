f85e51695c5a17aa81ed9d3e5e38e112831c1669
anaslag <anas.lagnani@gmail.com>
2023-04-13 14:58:35 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,7 +6,7 @@
  * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
- * Return: pointer to the memory area 
+ * Return: pointer to the memory area
  */
 char *_memset(char *s, char b, unsigned int n)
 {
