3d4589fd4fc1d9047ac897389dfce4e904f3c605
anaslag <anas.lagnani@gmail.com>
2023-05-02 15:27:31 -0700

    project singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (x = 0; temp && x < idx; x++)
 	{
-		if (x == idx - 1)
+		if (x == (idx - 1))
 		{
 			new->next = temp->next;
 			temp->next = new;
